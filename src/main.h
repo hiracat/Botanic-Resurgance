421a564908ec7d72fcb4b55fbafc7e9d3855b7e0
forest lehrman <forestgithub@skiff.com>
2023-10-22 14:54:00 -0400

    added a project level namespace and changed the order of function definitions

--- src/main.h
+++ src/main.h
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
-
+namespace btr{
 constexpr uint32_t WINDOW_WIDTH = 800;
 constexpr uint32_t WINDOW_HEIGHT = 600;
 struct App {
@@ -29,4 +29,11 @@ void mainLoop(GLFWwindow** window);
 void cleanup(App* app);
 bool checkValidationLayerSupport(const std::vector<const char*>& validationLayers);
 void setupDebugMessenger(App* app);
+void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                             const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+                                      const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) ;
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) ;
 std::vector<const char*> getRequiredExtentions();
+}
